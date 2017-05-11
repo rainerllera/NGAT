166b10763c6c448a44a51d8cf97936a8ce508207
Rainer Llera <rainerllera@gmail.com>
2017-05-11 12:32:41 -0400

    Adding default base class Entity

---
+++ NGAT.Domain/Domain/Base/Entity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NGAT.Business.Domain.Base
+{
+    /// <summary>
+    /// A Generic Entity, with id type <see cref="TKey"/>
+    /// </summary>
+    /// <typeparam name="TKey">The type of the id</typeparam>
+    public class Entity<TKey> where TKey:struct
+    {
+        /// <summary>
+        /// Id of the Entity
+        /// </summary>
+        public TKey Id { get; set; }
+    }
+
+    /// <summary>
+    /// Default Entity, with ulong as type for the Id
+    /// </summary>
+    public class Entity :Entity<ulong>
+    {
+
+    }
+}
