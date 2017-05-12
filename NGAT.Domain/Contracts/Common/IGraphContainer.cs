2bf36a64a9b826c03b5d7f8b454fe223a97c43a9
Rainer Llera <rainerllera@gmail.com>
2017-05-12 10:17:32 -0400

    Added IGraphContainer interface to represent arguments to algorithms or components that requiere a graph as input (or output)

---
+++ NGAT.Domain/Contracts/Common/IGraphContainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NGAT.Business.Domain.Core;
+
+namespace NGAT.Business.Contracts.Common
+{
+    /// <summary>
+    /// Represents a Type that encloses a Graph object
+    /// </summary>
+    public interface IGraphContainer
+    {
+        /// <summary>
+        /// The graph this object wraps
+        /// </summary>
+        Graph Graph { get; set; }
+    }
+}
