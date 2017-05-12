5f1266fd50f7c73d7f2c1b5f61fd636174519ad7
Rainer Llera <rainerllera@gmail.com>
2017-05-12 10:25:42 -0400

    Added IGraphExporter interface, to represent an object that exports a graph to a specific format

---
+++ NGAT.Domain/Contracts/IO/IGraphExporter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NGAT.Business.Contracts.Common;
+using System.Threading.Tasks;
+
+namespace NGAT.Business.Contracts.IO
+{
+    /// <summary>
+    /// Represents an object that exports (or saves) a graph to a specific format
+    /// </summary>
+    /// <typeparam name="TOutput"></typeparam>
+    public interface IGraphExporter<TInput> where TInput:IGraphContainer
+    {
+        /// <summary>
+        /// Exports a graph synchronously
+        /// </summary>
+        /// <param name="input">The input for this export, including the graph to export</param>
+        void Export(TInput input);
+
+        /// <summary>
+        /// Exports a Graph asynchronously
+        /// </summary>
+        /// <param name="input">The input for this export, including the graph to export</param>
+        /// <returns>A task that exports the graph</returns>
+        Task ExportAsync(TInput input);
+    }
+}
