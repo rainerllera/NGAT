340ad797d17f7454ca11bd093ae39dde9e8f5b9f
Rainer Llera <rainerllera@gmail.com>
2017-05-13 12:58:26 -0400

    Added DefaultOsmPbfGraphBuilder class, pending implementation
    Performed some refactoring

---
+++ NGAT.Business.Implementation/IO/Osm/DefaultOsmPbfGraphBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using NGAT.Business.Contracts.IO;
+using NGAT.Business.Domain.Core;
+using NGAT.Business.Implementation.IO.Osm.Inputs;
+
+namespace NGAT.Business.Implementation.IO.Osm
+{
+    public class DefaultOsmPbfGraphBuilder : IGraphBuilder<DefaultOsmPbfGraphBuilderInput>
+    {
+        public Graph Build(DefaultOsmPbfGraphBuilderInput input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Graph> BuildAsync(DefaultOsmPbfGraphBuilderInput input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- NGAT.Business.Implementation/IO/Osm/Inputs/DefaultOsmPbfGraphBuilderInput.cs
+++ NGAT.Business.Implementation/IO/Osm/Inputs/DefaultOsmPbfGraphBuilderInput.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using NGAT.Business.Contracts.Filters;
 
 namespace NGAT.Business.Implementation.IO.Osm.Inputs
