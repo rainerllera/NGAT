378d110ca8911a86103dd78feb05a52acebf9288
Rainer Llera <rainerllera@gmail.com>
2017-05-13 12:21:52 -0400

    Added INodeAttributeFilter interface, to represent a node filterer by attributes

---
+++ NGAT.Domain/Contracts/Filters/INodeAttributeFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NGAT.Business.Contracts.Filters
+{
+    /// <summary>
+    /// Abstract representation of a Node Filter by Attributtes.
+    /// Filters a node by performing filtering operations to its attributes.
+    /// </summary>
+    public interface INodeAttributeFilter
+    {
+        bool Filter(IDictionary<string, string> attributes);
+    }
+}
