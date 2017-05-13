6bb5a25f249b76dfbb5c801f86e181e82ed3ebb5
Rainer Llera <rainerllera@gmail.com>
2017-05-13 12:40:41 -0400

    Added IAttributesFetcher interface, and IAttributesFetcherCollection interface, to represent the fetchers of a graph objectt attributes

---
+++ NGAT.Domain/Contracts/Filters/IAttributesFetcher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NGAT.Business.Contracts.Filters
+{
+    /// <summary>
+    /// Represents an attributes fetcher
+    /// </summary>
+    public interface IAttributesFetcher
+    {
+        /// <summary>
+        /// A white list of attributes to fetch from an object's attribute (or field) collection
+        /// </summary>
+        IEnumerable<string> AttributesToFetch { get; set; }
+    }
+}
