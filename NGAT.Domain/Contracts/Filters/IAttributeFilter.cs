8c670dd3d9595416b3b9a01392f3bb06bfcc315e
Rainer Llera <rainerllera@gmail.com>
2017-05-13 12:43:21 -0400

    Added INodeAttributeFilter and INodeFilterCollection, to represent Filters

--- NGAT.Domain/Contracts/Filters/IAttributeFilter.cs
+++ NGAT.Domain/Contracts/Filters/IAttributeFilter.cs
@@ -8,8 +8,13 @@ namespace NGAT.Business.Contracts.Filters
     /// Abstract representation of a Node Filter by Attributtes.
     /// Filters a node by performing filtering operations to its attributes.
     /// </summary>
-    public interface INodeAttributeFilter
+    public interface IAttributeFilter
     {
+        /// <summary>
+        /// Returns true if the object with <paramref name="attributes"/> passes this filter
+        /// </summary>
+        /// <param name="attributes">The attributes to analyze</param>
+        /// <returns></returns>
         bool Filter(IDictionary<string, string> attributes);
     }
 }
