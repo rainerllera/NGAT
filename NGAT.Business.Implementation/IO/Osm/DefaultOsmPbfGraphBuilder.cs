7010ca1557ac48120a8b0036799c45afe7d48a2e
Rainer Llera <rainerllera@gmail.com>
2017-05-15 11:11:38 -0400

    Implemented adding nodes feature for DefaultOsmPbfGraphBuilder, pending adding arcs feature

--- NGAT.Business.Implementation/IO/Osm/DefaultOsmPbfGraphBuilder.cs
+++ NGAT.Business.Implementation/IO/Osm/DefaultOsmPbfGraphBuilder.cs
@@ -3,7 +3,11 @@ using System.Threading.Tasks;
 using NGAT.Business.Contracts.IO;
 using NGAT.Business.Domain.Core;
 using NGAT.Business.Implementation.IO.Osm.Inputs;
-using OsmSharp.IO.PBF;
+using System.Collections.Generic;
+using System.IO;
+using OsmSharp.Streams;
+using OsmSharp;
+using System.Linq;
 
 namespace NGAT.Business.Implementation.IO.Osm
 {
@@ -21,7 +25,52 @@ namespace NGAT.Business.Implementation.IO.Osm
 
         private Graph InternalBuild(DefaultOsmPbfGraphBuilderInput input)
         {
+            if (!File.Exists(input.FilePath))
+                throw new ArgumentException("Pbf file specified is invalid or doesn't exists.");
 
+            Graph result = new Graph();
+
+            using (var fileStream = File.OpenRead(input.FilePath))
+            {
+                var streamSource = new PBFOsmStreamSource(fileStream);
+
+                #region Adding Nodes (filtered)
+                foreach (OsmSharp.Node osmNode in streamSource.Where(o=>o.Type == OsmGeoType.Node))
+                {
+                    #region Reading Node Attributes
+                    IDictionary<string, string> attributes = new Dictionary<string, string>();
+                    foreach (var tag in osmNode.Tags)
+                    {
+                        attributes.Add(tag.Key, tag.Value);
+                    }
+                    #endregion
+
+                    #region Filtering node by its attributes
+                    if (input.NodeFiltersCollection.ApplyAllFilters(attributes) && osmNode.Longitude.HasValue && osmNode.Latitude.HasValue && osmNode.Id.HasValue)
+                    {
+                        var newNode = new Domain.Core.Node()
+                        {
+                            Latitude = osmNode.Latitude.Value,
+                            Longitude = osmNode.Longitude.Value,
+
+                        };
+
+                        //Fetching node attributes
+                        var fecthedAttributes = input.NodeAttributeFetchersCollection.FetchWhiteListed(attributes);
+
+                        //Adding the node to the graph
+                        result.AddNode(newNode, osmNode.Id.Value, fecthedAttributes);
+                    }
+                    #endregion
+                }
+                #endregion
+
+                #region Adding Arcs
+
+                #endregion
+            }
+
+            return result;
         }
     }
 }
