8d0207385b087d0cc8d08748d0de686b4c2562a6
Rainer Llera <rainerllera@gmail.com>
2017-05-15 18:15:22 -0400

    Added ArcData entity to avoid repeating memory consuming data for arcs that belong to the same topological way

---
+++ NGAT.Domain/Domain/Core/ArcData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NGAT.Business.Domain.Base;
+using Newtonsoft.Json;
+
+namespace NGAT.Business.Domain.Core
+{
+    public class ArcData : Entity<int>
+    {
+        public IDictionary<string, string> Attributes { get
+            {
+                return JsonConvert.DeserializeObject<Dictionary<string, string>>(this.RawData);
+            }
+        }
+
+        public string RawData { get; set; }
+    }
+}
