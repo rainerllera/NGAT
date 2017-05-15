d4eb45daa71dea47d091f699ed5861e8eb5a8de4
Rainer Llera <rainerllera@gmail.com>
2017-05-15 16:53:37 -0400

    Added DefaultOsmPbfGraphBuilder_Build_Tests and finished setting up mock objects

---
+++ NGAT.Business.Tests/GraphBuilders/Osm/DefaultOsmPbfGraphBuilderTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using NGAT.Business.Contracts.Filters;
+
+namespace NGAT.Business.Tests.GraphBuilders.Osm
+{
+    public class DefaultOsmPbfGraphBuilderTests
+    {
+        
+        public DefaultOsmPbfGraphBuilderTests()
+        {
+            var mockNodeFilterCollection = new Mock<IAttributeFilterCollection>();
+            mockNodeFilterCollection.Setup(i => i.ApplyAllFilters(It.IsAny<IDictionary<string, string>>())).Returns(true);
+            NodeFilterCollection = mockNodeFilterCollection.Object;
+
+            var mockNodeFetchersCollection = new Mock<IAttributesFetcherCollection>();
+            mockNodeFetchersCollection.Setup(i => i.FetchWhiteListed(It.IsAny<IDictionary<string, string>>()))
+                .Returns<IDictionary<string, string>>(i =>
+                {
+                    var result = new Dictionary<string, string>();
+                    if (i.ContainsKey("name"))
+                        result.Add("name", i["name"]);
+                    return result;
+                });
+
+            var mockArcFilterCollection = new Mock<IAttributeFilterCollection>();
+            mockArcFilterCollection.Setup(i => i.ApplyAllFilters(It.IsAny<IDictionary<string, string>>()))
+                .Returns<IDictionary<string,string>>((i)=> {
+                    
+                    return i.ContainsKey("highway");
+                });
+            ArcFilterCollection = mockArcFilterCollection.Object;
+
+            var mockArcFetchersColletciont = new Mock<IAttributesFetcherCollection>();
+            mockArcFetchersColletciont.Setup(i => i.FetchWhiteListed(It.IsAny<IDictionary<string, string>>()))
+                .Returns<IDictionary<string, string>>(i =>
+                {
+                    var result = new Dictionary<string, string>();
+                    foreach (var KV in i)
+                    {
+                        if (KV.Key.ToLowerInvariant().StartsWith("highway"))
+                            result.Add(KV.Key, KV.Value);
+                        if (KV.Key.ToLowerInvariant().StartsWith("oneway"))
+                            result.Add(KV.Key, KV.Value);
+                        if (KV.Key.ToLowerInvariant().Contains("way"))
+                            result.Add(KV.Key, KV.Value);
+                    }
+
+                    return result;
+                });
+            ArcFetchersCollection = mockArcFetchersColletciont.Object;
+
+        }
+
+        IAttributeFilterCollection NodeFilterCollection { get; set; }
+
+        IAttributeFilterCollection ArcFilterCollection { get; set; }
+
+        IAttributesFetcherCollection NodeFetchersCollection { get; set; }
+
+        IAttributesFetcherCollection ArcFetchersCollection { get; set; }
+
+        public void DefaultOsmPbfGraphBuilder_Build_Tests()
+        {
+
+        }
+    }
+}
