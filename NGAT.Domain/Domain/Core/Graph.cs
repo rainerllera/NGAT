c132b6966c25309a91dc4a31f5b07ff393295e3b
Rainer Llera <rainerllera@gmail.com>
2017-05-15 11:03:05 -0400

    Added AddNode method to Graph class

--- NGAT.Domain/Domain/Core/Graph.cs
+++ NGAT.Domain/Domain/Core/Graph.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using NGAT.Business.Domain.Base;
+using Newtonsoft.Json;
 
 namespace NGAT.Business.Domain.Core
 {
@@ -42,6 +43,29 @@ namespace NGAT.Business.Domain.Core
         /// The Arcs of this graph
         /// </summary>
         public virtual ICollection<Arc> Arcs { get; set; }
+        #endregion
+
+        #region Methods
+        public void AddNode(Node node, long originalId, IDictionary<string, string> fetchedAttributes)
+        {
+            if(!this.NodesIndex.ContainsKey(node.Coordinate.GetHashCode()))
+            {
+                //Using the well-formed hash code of the coordinate as Id for the node
+                node.Id = node.Coordinate.GetHashCode();
+
+                //Converting the fetched attributes for the node to Json and storing it
+                node.NodeData = JsonConvert.SerializeObject(fetchedAttributes);
+
+                //Storing the mapping
+                this.VertexToNodesIndex.Add(originalId, node.Id);
+
+                //Saving the node in collection and index
+                NodesIndex.Add(node.Id, node);
+                Nodes.Add(node);
+
+
+            }
+        }
         #endregion
     }
 }
