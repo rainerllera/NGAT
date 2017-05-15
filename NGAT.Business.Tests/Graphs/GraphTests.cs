b81f526ea64326dcc01269a6feec5d2d4555b466
Rainer Llera <rainerllera@gmail.com>
2017-05-15 14:25:35 -0400

    Fixed test Graph_AddNode_Tests for using the new Id of the nodes

--- NGAT.Business.Tests/Graphs/GraphTests.cs
+++ NGAT.Business.Tests/Graphs/GraphTests.cs
@@ -7,6 +7,7 @@ using GeoCoordinatePortable;
 
 namespace NGAT.Business.Tests.Graphs
 {
+ 
     public class GraphTests
     {
         [Fact]
@@ -15,7 +16,7 @@ namespace NGAT.Business.Tests.Graphs
             var graph = new Graph();
 
             var coordinate = new GeoCoordinate(2, 2);
-            var id = coordinate.GetHashCode();
+            var id = 1;
 
             var newNode = new Node()
             {
@@ -41,6 +42,62 @@ namespace NGAT.Business.Tests.Graphs
 
             Assert.True(attrsDeserialized["name"] == "NodeName");
             Assert.True(attrsDeserialized["age"] == "64");
+        }
+
+        [Fact]
+        public void Graph_AddArc_Tests()
+        {
+            var graph = new Graph();
+
+            //Adding 10 nodes
+            for (int i = 1; i < 11; i++)
+            {
+                var coordinate = new GeoCoordinate(i, i);
+                var id = coordinate.GetHashCode();
+
+                var newNode = new Node()
+                {
+                    Latitude = i,
+                    Longitude = i
+                };
+                long originalId = (long)i;
+
+                var fetchedAttributes = new Dictionary<string, string>();
+                fetchedAttributes.Add("name", "NodeName" + i.ToString());
+                fetchedAttributes.Add("age", i.ToString());
+
+                graph.AddNode(newNode, originalId, fetchedAttributes);
+            }
+
+            Assert.True(graph.Nodes.Count == 10);
+            Assert.True(graph.VertexToNodesIndex.Count == 10);
+            Dictionary<string, string> fetchedattrs = new Dictionary<string, string>();
+            graph.AddArc(1, 3, fetchedattrs);
+            graph.AddArc(1, 4, fetchedattrs);
+            graph.AddArc(4, 3, fetchedattrs);
+            graph.AddArc(4, 1, fetchedattrs);
+            graph.AddArc(3, 6, fetchedattrs);
+
+            //Verifying arcs were added correctly
+            Assert.True(graph.ArcsIndex[1].FromNodeId == graph.VertexToNodesIndex[1] && graph.ArcsIndex[1].ToNodeId == graph.VertexToNodesIndex[3]);
+            Assert.True(graph.ArcsIndex[2].FromNodeId == graph.VertexToNodesIndex[1] && graph.ArcsIndex[2].ToNodeId == graph.VertexToNodesIndex[4]);
+            Assert.True(graph.ArcsIndex[3].FromNodeId == graph.VertexToNodesIndex[4] && graph.ArcsIndex[3].ToNodeId == graph.VertexToNodesIndex[3]);
+            Assert.True(graph.ArcsIndex[4].FromNodeId == graph.VertexToNodesIndex[4] && graph.ArcsIndex[4].ToNodeId == graph.VertexToNodesIndex[1]);
+            Assert.True(graph.ArcsIndex[5].FromNodeId == graph.VertexToNodesIndex[3] && graph.ArcsIndex[5].ToNodeId == graph.VertexToNodesIndex[6]);
+
+            //Verifying relations were added correctly
+            Assert.True(graph.NodesIndex[1].OutgoingArcs.Count == 2);
+            Assert.True(graph.NodesIndex[1].OutgoingArcs[0].ToNodeId == 3);
+            Assert.True(graph.NodesIndex[1].OutgoingArcs[1].ToNodeId == 4);
+            Assert.True(graph.NodesIndex[1].IncomingArcs.Count == 1);
+            Assert.True(graph.NodesIndex[1].IncomingArcs[0].FromNodeId == 4);
+
+            //testing distances
+            graph.AddArc(1, 6, 5000, fetchedattrs);
+
+            Assert.True(graph.ArcsIndex[6].Distance == 5000);
+            Assert.True(graph.ArcsIndex[2].Distance == graph.ArcsIndex[4].Distance);
+
         }
     }
 }
