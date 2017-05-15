34e5abf0a8d8b6f6ce79ac64d472febaf84c3d0c
Rainer Llera <rainerllera@gmail.com>
2017-05-15 11:29:54 -0400

    Added Tests for Graph_AddNode_Tests, passed

---
+++ NGAT.Business.Tests/Graphs/GraphTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using NGAT.Business.Domain.Core;
+using GeoCoordinatePortable;
+
+namespace NGAT.Business.Tests.Graphs
+{
+    public class GraphTests
+    {
+        [Fact]
+        public void Graph_AddNode_Tests()
+        {
+            var graph = new Graph();
+
+            var coordinate = new GeoCoordinate(2, 2);
+            var id = coordinate.GetHashCode();
+
+            var newNode = new Node()
+            {
+                Latitude = 2,
+                Longitude = 2
+            };
+            long originalId = 289912;
+
+            var fetchedAttributes = new Dictionary<string, string>();
+            fetchedAttributes.Add("name", "NodeName");
+            fetchedAttributes.Add("age", "64");
+
+            graph.AddNode(newNode, originalId, fetchedAttributes);
+
+            Assert.True(graph.VertexToNodesIndex.ContainsKey(originalId));
+            Assert.True(graph.VertexToNodesIndex[originalId] == id);
+            Assert.True(newNode.Id == id);
+            Assert.True(graph.NodesIndex.ContainsKey(id) && graph.NodesIndex[id] == newNode);
+
+            Assert.True(newNode.NodeData != null);
+
+            var attrsDeserialized = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, string>>(newNode.NodeData);
+
+            Assert.True(attrsDeserialized["name"] == "NodeName");
+            Assert.True(attrsDeserialized["age"] == "64");
+        }
+    }
+}
