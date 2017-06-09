db01771da92de7d2c7230a2b93b6f844558f9287
Rainer Llera <rainerllera@gmail.com>
2017-06-08 20:55:42 -0400

    Building DataAccess Layer

---
+++ NGAT.DataAccess/File/Graphs/FileGraphsRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NGAT.DataAccess.File.Graphs
+{
+    class FileGraphsRepository
+    {
+    }
+}
---
+++ NGAT.DataAccess/Sql/SqlRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NGAT.DataAccess.Sql
+{
+    class SqlRepository
+    {
+    }
+}
